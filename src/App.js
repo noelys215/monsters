d7817c6e71fb926a44fbc17ac5514856b87fead9
Henry B <noelys215@gmail.com>
2022-08-14 20:23:48 -0400

    add filter

--- src/App.js
+++ src/App.js
@@ -6,29 +6,49 @@ class App extends Component {
 		super();
 		this.state = {
 			monsters: [],
+			searchField: '',
 		};
+		console.log('constructor');
 	}
 
 	componentDidMount() {
+		console.log('componentDidMount');
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((res) => res.json())
 			.then((users) =>
 				this.setState(
 					() => {
 						return { monsters: users };
-					},
-					() => {
-						console.log(this.state);
 					}
+					// () => console.log(this.state)
 				)
 			)
 			.catch((err) => console.log(err.message));
 	}
 
 	render() {
+		console.log('Render');
+
+		const filteredMonsters = this.state.monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(this.state.searchField)
+		);
+
 		return (
 			<div className="App">
-				{this.state.monsters.map((monster) => {
+				<input
+					className="search-box"
+					type="search"
+					placeholder="Search Monster.."
+					onChange={(e) => {
+						const searchField = e.target.value.toLowerCase();
+
+						this.setState(() => {
+							return { searchField };
+						});
+					}}
+				/>
+
+				{filteredMonsters.map((monster) => {
 					return (
 						<div key={monster.id}>
 							<h1>{`${monster.name}`}</h1>
